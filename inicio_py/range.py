7329f657b5a2b0440d861d41ab5fc550b4871a04
Michel Gabriel da Silva <michelgbr79@gmail.com>
2024-05-10 00:16:52 -0300

    Delete inicio_py directory

--- inicio_py/range.py
+++
@@ -1,8 +0,0 @@
-#range é uma funcao que utilizamos (start , stop, step)
-#start é o inivio, stop é o numero limte e step é de qnts numeros vc quer pular
-
-numeros = range(1 , 20 , 2)
-
-for valor in numeros:
-    
-    print(valor)
