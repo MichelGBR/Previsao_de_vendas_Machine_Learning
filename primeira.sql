3065c395fe06f56c16a43d0e49ce8384b6590a79
Michel Gabriel da Silva <michelgbr79@gmail.com>
2024-06-27 17:19:00 -0300

    Delete primeira.sql

--- primeira.sql
+++
@@ -1,4 +0,0 @@
-insert into df(`Localidade`,soma_de_vendas_por_estado) values('Centro',109587);
-insert into df(`Localidade`,soma_de_vendas_por_estado) values('Oeste',101507);
-insert into df(`Localidade`,soma_de_vendas_por_estado) values('Leste',67501);
-insert into df(`Localidade`,soma_de_vendas_por_estado) values('Sul',63727);
