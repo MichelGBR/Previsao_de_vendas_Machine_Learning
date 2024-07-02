e080c5c8362a8060e0d4f0cc27aa3c1e614065c7
Michel Gabriel da Silva <michelgbr79@gmail.com>
2024-07-02 19:31:30 -0300

    Add perg4 SQL

---
+++ Perguntas_de_negócio/pergunta4/pergunta4.sql
@@ -0,0 +1,11 @@
+# 1 - Qual o número total de vendas por localidade?
+
+
+# 4 - Quantos mercados estão abaixo da média em marketing por localidade? 
+
+SELECT localidade, COUNT (investimento_em_marketing) AS qtd_abaixo_da_media
+FROM newdf
+WHERE investimento_em_marketing <16
+GROUP BY localidade
+ORDER BY qtd_abaixo_da_media DESC
+LIMIT 1000;
