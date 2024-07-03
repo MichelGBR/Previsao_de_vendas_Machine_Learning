3aa5d1eeaed5ae337fa0b14b7b46ef5e0d76e9e3
Michel Gabriel da Silva <michelgbr79@gmail.com>
2024-07-03 15:14:22 -0300

    Delete Perguntas_de_negócio/perguntas6 directory

--- Perguntas_de_negócio/perguntas6/pergunta06.sql
+++
@@ -1,8 +0,0 @@
-# 6 - Quantos mercados estão abaixo da média de vendas por localidade? 
-
-SELECT localidade, COUNT (vendas) AS qtd_abaixo_da_media
-FROM newdf
-WHERE vendas <124
-GROUP BY localidade
-ORDER BY qtd_abaixo_da_media DESC
-LIMIT 1000;
